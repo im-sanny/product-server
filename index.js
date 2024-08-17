86a6c9afefc21c67dbb79602f3a20680679b6a96
im-sanny <raselparvezsanny@gmail.com>
2024-08-17 15:25:27 +0600

    add: mongodb added

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
@@ -6,17 +7,36 @@ const port = process.env.PORT || 9000;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173, http://localhost:5174"],
+  origin: ["http://localhost:5173", "http://localhost:5174"],
   credentials: true,
   optionSuccessStatus: 200,
-}
+};
 
 app.use(cors(corsOptions));
-app.use(express.json())
+app.use(express.json());
 
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.96corz1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
-app.get('/', (res, req) =>{
-    res.setEncoding('Hello form ProductHub server...')
-})
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
+});
 
-app.listen(port, () =>console.log(`server running on port ${port}`))
+async function run() {
+  try {
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+  }
+}
+
+run().catch(console.dir);
+
+app.get("/", (req, res) => {
+  res.send("Hello from ProductHub server...");
+});
+
+app.listen(port, () => console.log(`Server running on port ${port}`));
