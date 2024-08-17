0cc079e7a95d014f9b3df148ed504b6e70467f00
im-sanny <raselparvezsanny@gmail.com>
2024-08-17 19:50:58 +0600

    deploy: project deployed successfully

--- index.js
+++ index.js
@@ -7,11 +7,10 @@ const port = process.env.PORT || 9000;
 const app = express();
 
 const corsOptions = {
-  origin: ["http://localhost:5173", "http://localhost:5174"],
+  origin: ["http://localhost:5173", "https://producthub-13705.web.app"],
   credentials: true,
   optionSuccessStatus: 200,
 };
-
 app.use(cors(corsOptions));
 app.use(express.json());
 
